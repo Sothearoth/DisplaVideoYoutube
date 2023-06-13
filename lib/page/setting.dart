5625c640b7f462afefc3deec89b93551348edd35
apple <naronorngkiller@gmail.com>
2023-06-13 21:44:18 +0700

    Upload Image

--- lib/page/setting.dart
+++ lib/page/setting.dart
@@ -1,3 +1,4 @@
+
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
@@ -9,6 +10,7 @@ import 'package:flutter_application_1/utils/navigate_custom.dart';
 import 'package:flutter_application_1/widget/popup_dialog.dart';
 import 'package:flutter_application_1/widget/sliver_appbar.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -23,6 +25,7 @@ class _SettingScreenState extends State<SettingScreen> {
     {"title": "Change Languguage", "icon": Icons.language},
     {"title": "About Us", "icon": Icons.info},
     {"title": "Logout", "icon": Icons.logout},
+    {"title": "Upload Image", "icon": Icons.upload},
   ];
 
   File? _image;
@@ -69,11 +72,16 @@ class _SettingScreenState extends State<SettingScreen> {
                       PopupSetting().showSettingAlertDialog(context, "Change Language");
                     }else if (index == 1){
                       // about us click
-                    }else{
+                    }else if(index == 2){
                       AppPreference.clearLogin();
                       navigateTo(context, destination: const LoginScreen());
+                    }else if (index == 3){
+                      if(_image != null){
+                        uploadImage(_image?.path);
+                      }else{
+                        print('Not pick image');
+                      }
                     }
-
                   },
                   child: Container(
                     margin: const EdgeInsets.only(top: 10),
@@ -126,6 +134,28 @@ class _SettingScreenState extends State<SettingScreen> {
       ),
     );
   }
+
+
+
+  Future<void> uploadImage(filepath) async {
+
+      var request = http.MultipartRequest('POST', Uri.parse('https://qacltom.udaya-tech.com/MacleLogistic/user/edit-user'));
+      request.headers['Authorization'] = 'bearer fbf9dded-8ff3-4b79-860c-a974b57489aa';
+      request.headers['Content-Type'] = 'multipart/form-data';
+      request.files.add(await http.MultipartFile.fromPath('file', filepath));
+
+      request.send().then((value) => {
+        http.Response.fromStream(value).then((value) => {
+          if(value.statusCode == 200){
+            print(value.body)
+          }else{
+             throw Exception('Fail to load to server')
+          }
+        })
+      });
+  }
+
+
 
   static Future<File?> imageChooseDialog(context) async {
 
